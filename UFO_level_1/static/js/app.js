96db0f452aff3a91cef744422197cfbc4fcb6d3e
Shannan <sharpshannan60@gmail.com>
2021-06-18 16:57:17 -0500

    added code on app.js

--- UFO_level_1/static/js/app.js
+++ UFO_level_1/static/js/app.js
@@ -1,4 +1,43 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+
+console.log(tableData);
+
+var tbody = d3.select("tbody");
+console.log(data);
+
+// data.forEach(function(ufoSighting){
+//     console.log(ufoSighting);
+
+// });
+
+data.forEach(function(ufoSighting){
+    console.log(ufoSighting);
+    var row = tbody.append("tr");
+    Object.entries(ufoSighting).forEach(function([key,value]){
+        console.log(key,value);
+        var cell = tbody.append("td");
+        cell.text(value);
+    });
+});
+
+var button = d3.select("#filter-btn");
+
+button.on("click", function(event){
+    d3.event.preventDefault();
+    tbody.html("");
+
+var inputElement = d3.select("#datetime"); 
+var inputValue = inputElement.property("value");
+
+var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+filteredData.forEach(function(dateData){
+    var row=tbody.append("tr");
+    Object.entries(dateData).forEach(function([key,value]){
+    var cell=tbody.append("td");
+    cell.text(value);
+        });
+    });
+});
+
